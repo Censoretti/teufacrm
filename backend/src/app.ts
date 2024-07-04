36a24ec47a1819aa791ae955f6f6743b8b5d3c4e
paiotti <rodrigopaiotti96@gmail.com>
2024-07-04 16:40:13 -0300

    minor ajust

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api', productRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
